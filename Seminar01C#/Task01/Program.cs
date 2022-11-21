0060bff1327560ec10649b1b665a4a3c265739ed
warlordsxp <warlordsxp@yandex.ru>
2022-11-21 10:22:19 +0300

    Проверка на ввод 2-хзначного числа

--- Seminar01C#/Task01/Program.cs
+++ Seminar01C#/Task01/Program.cs
@@ -3,3 +3,8 @@ int num = Convert.ToInt32(Console.ReadLine());
 int square = num*num;
 //Console.WriteLine("Квадрат введеного числа равен "+square);
 Console.WriteLine($"Квадрат введеного числа равен {square}");
+
+if (num>9)
+{
+    Console.WriteLine("Вы ввели двухзначное число!");
+}
