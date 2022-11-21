8d63053a4a4f4da71d039506671f94397b9f49ed
warlordsxp <warlordsxp@yandex.ru>
2022-11-21 11:26:56 +0300

    Добавлен Task01 по ДЗ задачи 2-ой

---
+++ Seminar01C#/HomeWork/Task01.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 1-ое целое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое целое число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+        Console.WriteLine($"Max={num1} Min={num2}");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"Max={num2} Min={num1}");
+}
+else
+{
+    Console.WriteLine($"Max & Min = {num1}");
+}
