2a4ad80077827b29b4de153e2ddeed2d2b474884
warlordsxp <warlordsxp@yandex.ru>
2022-11-21 10:48:45 +0300

    Добавили Вывод всех числе от введеного до 25

--- Seminar01C#/Task01/Program.cs
+++ Seminar01C#/Task01/Program.cs
@@ -4,7 +4,14 @@ int square = num*num;
 //Console.WriteLine("Квадрат введеного числа равен "+square);
 Console.WriteLine($"Квадрат введеного числа равен {square}");
 
-if (num>9)
+if (num>9 && num<100) // проверка на 2-хзначное число и выше
 {
     Console.WriteLine("Вы ввели двухзначное число!");
 }
+
+while(num<=25) // вывод всех числе от введеного до 25
+{
+    Console.Write($"{num} ");
+    num++;
+}
+Console.WriteLine();
