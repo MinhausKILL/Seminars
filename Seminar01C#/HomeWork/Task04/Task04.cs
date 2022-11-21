92a25a5c8a8c3a57b48eaa18f655b52c90a67be1
warlordsxp <warlordsxp@yandex.ru>
2022-11-21 11:40:44 +0300

    Перенесены файлы задачи 2 в Task02 и создана папка Task04 задачи 4

---
+++ Seminar01C#/HomeWork/Task04/Task04.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите 1-ое целое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое целое число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3-е целое число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+{
+        Console.WriteLine($"Max={num1}");
+}
+else if (num1 < num2 && num2 > num3)
+{
+    Console.WriteLine($"Max={num2}");
+}
+else
+{
+    Console.WriteLine($"Max={num3}");
+}
